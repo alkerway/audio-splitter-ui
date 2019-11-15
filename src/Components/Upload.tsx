a7ece35bb44752f0612540e45a25f8b96a14cfdd
alexre0 <alex@realeyes.com>
2019-11-15 22:48:01 +1300

    check status functionalityD

--- src/Components/Upload.tsx
+++ src/Components/Upload.tsx
@@ -1,24 +1,26 @@
 import * as React from 'react';
+import { Statuses } from '../Models/Statuses'
 
 export interface UploadState {
-    uploading: boolean;
+    status: Statuses;
     file: null | File;
-    uploaded: boolean;
+    statusInterval: NodeJS.Timer | null
 }
 
 export interface UploadProps {
 
 }
 const port = 8880
-const uploadEndpoint = `${window.location.protocol}//${window.location.hostname}:${port}/upload`
+const endpoint = `${window.location.protocol}//${window.location.hostname}:${port}`
 
 export class Upload extends React.Component<UploadProps, UploadState> {
+    private statusIntervalTime = 2000;
     constructor(props: UploadProps) {
         super(props)
         this.state ={
             file: null,
-            uploading: false,
-            uploaded: false
+            status: Statuses.UNINITIALIZED,
+            statusInterval: null
         }
     }
     onFileAdded = (fileList: FileList | null) => {
@@ -34,25 +36,72 @@ export class Upload extends React.Component<UploadProps, UploadState> {
         if (this.state.file) {
             this.setState({
                 ...this.state,
-                uploaded: false,
-                uploading: true
+                status: Statuses.UPLOADING
             })
             const form = new FormData()
             form.append('file', this.state.file)
-            fetch(uploadEndpoint,{
+            fetch(`${endpoint}/upload`,{
                 method: 'POST',
                 body: form
             }).then((res) => {
-                return res.text()
-            }).then((text) => {
-                console.log(text)
+                if (res.ok) {
+                    return res.json()
+                }
+            }).then((res) => {
+                this.setState({
+                    ...this.state,
+                    status: Statuses.UPLOADED
+                })
+                this.startStatusCheck(res.name)
+            }).catch((err) => {
+                console.log(err)
             })
         }
+    }
+    async getStatus (name: string): Promise<String> {
+        const response = await fetch(`${endpoint}/checkstatus`, {
+            method: 'POST',
+            body: JSON.stringify({name}),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        const responseObj = await response.json()
+        if (response.ok) {
+            return responseObj.status
+        } else {
+            throw new Error(responseObj.message)
+        }
+    }
+    startStatusCheck = (name: string) => {
+        this.setState({
+            ...this.state,
+            statusInterval: setInterval(() => {
+                this.getStatus(name).then((status) => {
+                    this.setState({
+                        ...this.state,
+                        status
+                    })
+                    if (status === Statuses.COMPLETE || status === Statuses.ERRORRED) {
+                        this.stopStatusCheck()
+                    }
+                }).catch(err => console.log)
+            }, this.statusIntervalTime)
+        })
+    }
+    stopStatusCheck = () => {
+        if (this.state.statusInterval) {
+            clearInterval(this.state.statusInterval)
+        }
+        this.setState({
+            ...this.state,
+            statusInterval: null
+        })
     }
     render() {
         return (
             <div className="uploadContainer">
-                {/*<div className="fileDisplay">{this.state.file && this.state.file.name}</div>*/}
+                <div className="fileDisplay">{this.state.file && this.state.file.name}</div>
                 <div className="fileInputWrapper">
                     <input
                         id="file-input"
@@ -64,6 +113,9 @@ export class Upload extends React.Component<UploadProps, UploadState> {
                 <button
                     onClick={this.startUpload}
                 >upload</button>
+                <div className="statusDisplay">
+                    {this.state.status !== Statuses.UNINITIALIZED ? `Status: ${this.state.status}` : ''}
+                </div>
             </div>
         )
     }
---
+++ src/Models/Statuses.ts
@@ -0,0 +1,10 @@
+export class Statuses {
+    public static UNINITIALIZED = 'UNINITIALIZED'
+    public static UPLOADING = 'UPLOADING'
+    public static UPLOADED = 'UPLOADED'
+    public static ERRORRED = "ERRORRED"
+    public static INITIALIZED = "INITIALIZED"
+    public static SPLITTING_AUDIO = "SPLITTING_AUDIO"
+    public static COMPLETE = "COMPLETE"
+    public static SENT = "SENT"
+}
