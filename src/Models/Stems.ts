6aa2e0ecb4a0aaefe1985ae5e1f643ee800d33e4
alexre0 <alex@realeyes.com>
2019-11-16 12:51:05 +1300

    checkbox work

--- src/Models/Stems.ts
+++ src/Models/Stems.ts
@@ -2,7 +2,7 @@ export type Stem = 2 | 4 | 5
 export type Stem2Option = 'vocals' | 'accompaniment'
 export type Stem4Option = 'vocals' | 'drums' | 'bass' | 'other'
 export type Stem5Option = Stem4Option | 'piano'
-export type SelectedOptions = Array<Stem extends 2 ? Stem2Option : Stem extends 4 ? Stem4Option : Stem5Option>
+export type SelectedOptions = Set<Stem extends 2 ? Stem2Option : Stem extends 4 ? Stem4Option : Stem5Option>
 
 export interface SpleeterOptions {
     stems: Stem,
