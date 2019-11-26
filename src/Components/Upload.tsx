b50ad165673ea9343352e0bbc642d60cab4b971e
alexre0 <alex@realeyes.com>
2019-11-27 10:54:58 +1300

    added http service and separated file input component

--- src/Components/Upload.tsx
+++ src/Components/Upload.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { Download } from "./Download";
+import {FileInput} from "./FileInput";
 import { Options }from './Options'
 import { Statuses } from '../Models/Statuses'
 import {SpleeterOptions} from "../Models/Stems";
+import HttpService from "../Services/http.service"
 
 export interface UploadState {
     status: Statuses;
@@ -12,11 +14,7 @@ export interface UploadState {
     options: SpleeterOptions
 }
 
-export interface UploadProps {
-
-}
-const port = 8880
-const endpoint = `${window.location.protocol}//${window.location.hostname}:${port}`
+export interface UploadProps {}
 
 export class Upload extends React.Component<UploadProps, UploadState> {
     private statusIntervalTime = 1000;
@@ -34,18 +32,12 @@ export class Upload extends React.Component<UploadProps, UploadState> {
             }
         }
     }
-    onFileAdded = (fileList: FileList | null) => {
-        if (fileList) {
-            this.setState({
-                ...this.state,
-                file: fileList[0]
-            })
+    onFileAdded = (file: File) => {
+            this.setState({...this.state, file })
             this.startUpload()
-        }
     }
     startUpload = () => {
-        if (this.state.file) {
-            if (this.state.options.isolate.size || this.state.options.remove.size) {
+        if (this.state.file && (this.state.options.isolate.size || this.state.options.remove.size)) {
                 this.setState({
                     ...this.state,
                     status: Statuses.UPLOADING
@@ -55,54 +47,50 @@ export class Upload extends React.Component<UploadProps, UploadState> {
                 form.append('stems', String(this.state.options.stems))
                 form.append('isolate', JSON.stringify(Array.from(this.state.options.isolate)))
                 form.append('remove', JSON.stringify(Array.from(this.state.options.remove)))
-                fetch(`${endpoint}/upload`, {
-                    method: 'POST',
-                    body: form
-                }).then((res) => {
-                    if (res.ok) {
-                        return res.json()
-                    }
-                }).then((res) => {
-                    this.setState({
-                        ...this.state,
-                        status: Statuses.UPLOADED,
-                        sessionname: res.name
+                HttpService.post('upload', form)
+                    .then((res) => {
+                        if (res) {
+                            this.setState({
+                                ...this.state,
+                                status: Statuses.UPLOADED,
+                                sessionname: res.name
+                            })
+                            this.startStatusCheck(res.name)
+                        }
                     })
-                    this.startStatusCheck(res.name)
-                }).catch((err) => {
-                    console.log(err)
-                })
             }
-        }
     }
-    async getStatus (name: string): Promise<String> {
-        const response = await fetch(`${endpoint}/checkstatus`, {
-            method: 'POST',
-            body: JSON.stringify({name}),
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
-        const responseObj = await response.json()
-        if (response.ok) {
-            return responseObj.status
-        } else {
-            throw new Error(responseObj.message)
+    async getStatus (name: string): Promise<Statuses | null> {
+        const headers: HeadersInit = {
+            'Content-Type': 'application/json'
         }
+        const response = await HttpService.post('checkstatus', JSON.stringify({name}), headers)
+        if (response && response.status) {
+            return response.status
+        }
+        return null;
     }
     startStatusCheck = (name: string) => {
         this.setState({
             ...this.state,
             statusInterval: setInterval(() => {
                 this.getStatus(name).then((status) => {
-                    this.setState({
-                        ...this.state,
-                        status
-                    })
-                    if (status === Statuses.COMPLETE || status === Statuses.ERRORED) {
+                    if (status) {
+                        this.setState({
+                            ...this.state,
+                            status
+                        })
+                        if (status === Statuses.COMPLETE || status === Statuses.ERRORED) {
+                            this.stopStatusCheck()
+                        }
+                    } else {
+                        this.setState({
+                            ...this.state,
+                            status: Statuses.ERRORED
+                        })
                         this.stopStatusCheck()
                     }
-                }).catch(err => console.log)
+                })
             }, this.statusIntervalTime)
         })
     }
@@ -116,31 +104,18 @@ export class Upload extends React.Component<UploadProps, UploadState> {
         })
     }
     onOptionsChange = (options: SpleeterOptions) => {
-        this.setState({
-            ...this.state,
-            options
-        })
+        this.setState({...this.state, options })
     }
     render() {
         return (
             <div className="uploadContainer">
-                <div className="fileDisplay">{this.state.file && this.state.file.name}</div>
                 <Options sendState={this.onOptionsChange}/>
-                <div className="fileInputWrapper">
-                    <input
-                        id="file-input"
-                        type="file"
-                        name="fileInput"
-                        accept="audio/*"
-                        onChange={(e) => this.onFileAdded(e.target.files)}/>
-                </div>
-                <button
-                    onClick={this.startUpload}
-                >upload</button>
+                <FileInput onFileAdded={this.onFileAdded}/>
+                <button onClick={this.startUpload}>upload</button>
                 <div className="statusDisplay">
                     {this.state.status !== Statuses.UNINITIALIZED ? `Status: ${this.state.status}` : ''}
                 </div>
-                <Download name={this.state.sessionname} endpoint={endpoint} status={this.state.status} />
+                <Download name={this.state.sessionname} status={this.state.status} />
             </div>
         )
     }
